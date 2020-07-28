e59c017a6632ad95a32c6a9f3526f26f534d44b1
lex-marie790 <lexi.marie790@gmail.com>
2020-07-27 23:33:55 -0700

    rendering correctly

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import './App.css';
-// import axios from 'axios';
+// import './App.css';
+
 
 import DoggoCard from './Components/DoggoCard.js';
 
 class App extends React.Component {
   state={
-    dogs: []
+    breeds: []
   };
 
   componentDidMount(){
     fetch('https://api.thedogapi.com/v1/images/search')
     .then(res => res.json())
     .then((data) => {
-      this.setState({ dogs: data })
+      this.setState({ breeds:data })
+      console.log('app = ' + data);
     })
     .catch(err => {
       console.log('cant get dogs')
     });
   }
+
+  handleSubmit
   
   render() {
     return(
       <div>
-        <DoggoCard dogs = {this.state.dogs} />
+        <DoggoCard key={this.state.breeds.id} breeds = {this.state.breeds} />
+        {/* <button onClick={this.state.breeds}>Get A New Doggo!</button> */}
+
       </div>
     )
   }
--- src/Components/DoggoCard.js
+++ src/Components/DoggoCard.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import '../App.css';
 
-const DoggoCard = ({ dogs }) => {
+// props = breeds from app.js
+
+const DoggoCard = ( props ) => {
+    console.log('doggocard' + props)
+
     return(
-        <>
-            {dogs.map((dog) => {
+        <div className='doggo'>
+            {props.breeds.map((dog, index) => {
+                console.log(dog.breeds)
                 return(
-                    <div key={dog.id}>
-                        <h1 >{dog.weight}</h1>
-                        <p>{dogs.id}</p>
-                        <h2 >{dog.name}</h2>
-                        <h3 >{dog.temperament}</h3>
-                        <img src = {dog.url}  />
-                </div>
+                    <div className='dog-card'>
+                        <Fragment key = {index}>
+                            <img src = {dog.url} alt='dog' />
+                            {/* [0] came from the array  */}
+                            <div className = 'card'>
+                                <h2>{dog.breeds[0].name}</h2>
+                                <h3>{dog.breeds[0].temperament}</h3>                               
+                            </div>                
+                            </Fragment>
+                    </div>
                 )}
             )}
-        </>
+        </div>
     )
 };
 
